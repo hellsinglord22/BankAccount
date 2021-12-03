587362d89b725a7ba3c3bc5c2e01e607b007b5c7
Hesham Adel <hesham.adel@speakol.com>
2021-12-03 04:05:35 +0200

    Creates inhertance

---
+++ Moataz2051710628/Withdraw.java
@@ -0,0 +1,8 @@
+package BankAccount.Moataz2051710628;
+
+public class Withdraw extends Transaction{
+
+    public Withdraw(Account from, Account to, float amount, String type, String state) {
+        super(from, to, amount, type, state);
+    }
+}
